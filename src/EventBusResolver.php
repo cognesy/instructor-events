64c6d9fa2be2eca41ac3aaeea43f9c19025f05c5
ddebowczyk <ddebowczyk@gmail.com>
2025-06-17 09:33:26 +0000

    Release version 1.0-rc3

---
+++ src/EventBusResolver.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Cognesy\Events;
+
+use Cognesy\Events\Contracts\CanHandleEvents;
+use Cognesy\Events\Dispatchers\EventDispatcher;
+use Psr\EventDispatcher\EventDispatcherInterface;
+
+class EventBusResolver implements CanHandleEvents
+{
+    private CanHandleEvents $eventHandler;
+
+    private function __construct(
+        ?CanHandleEvents $eventHandler
+    ) {
+        $this->eventHandler = match(true) {
+            is_null($eventHandler) => new EventDispatcher(),
+            $eventHandler instanceof EventBusResolver => $eventHandler->eventHandler, // avoid double wrapping
+            $eventHandler instanceof CanHandleEvents => $eventHandler, // CanHandleEvents implementation
+            $eventHandler instanceof EventDispatcherInterface =>  new EventDispatcher(parent: $eventHandler), // wrap with EventDispatcher
+            default => $eventHandler,
+        };
+    }
+
+    // FACTORY METHODS ////////////////////////////////////////////
+
+    public static function default(): self {
+        return new self(new EventDispatcher());
+    }
+
+    public static function using(?CanHandleEvents $events): self {
+        return new self($events);
+    }
+
+    // PUBLIC ////////////////////////////////////////////////////
+
+    public function wiretap(callable $listener): void {
+        $this->eventHandler->wiretap($listener);
+    }
+
+    public function addListener(string $name, callable $listener, int $priority = 0): void {
+        $this->eventHandler->addListener($name, $listener, $priority);
+    }
+
+    public function dispatch(object $event) : object{
+        return $this->eventHandler->dispatch($event);
+    }
+
+    public function getListenersForEvent(object $event): iterable {
+        return $this->eventHandler->getListenersForEvent($event);
+    }
+}
---
+++ src/Traits/HandlesEvents.php
@@ -0,0 +1,50 @@
+<?php
+namespace Cognesy\Events\Traits;
+
+use Cognesy\Events\Contracts\CanHandleEvents;
+use Cognesy\Events\Event;
+use Cognesy\Events\EventBusResolver;
+
+trait HandlesEvents
+{
+    protected CanHandleEvents $events;
+
+    public function withEventHandler(CanHandleEvents $events) : static {
+        $this->events = EventBusResolver::using($events);
+        return $this;
+    }
+
+    /**
+     * Dispatches an event
+     *
+     * @param Event $event The event to be emitted
+     */
+    public function dispatch(Event $event) : object {
+        return $this->events->dispatch($event);
+    }
+
+    /**
+     * Registers callback listening to all events
+     *
+     * @param callable $listener The listener callable to be invoked on any event
+     */
+    public function wiretap(?callable $listener) : self {
+        if ($listener !== null) {
+            $this->events->wiretap($listener);
+        }
+        return $this;
+    }
+
+    /**
+     * Registers callback listening to a specific event type
+     *
+     * @param string $class The event class
+     * @param callable $listener The listener callable to be invoked on event
+     */
+    public function onEvent(string $class, ?callable $listener) : self {
+        if ($listener !== null) {
+            $this->events->addListener($class, $listener);
+        }
+        return $this;
+    }
+}
